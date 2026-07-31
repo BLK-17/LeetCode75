2b5202c25adb52ad41d0f43104b78df99b0420e6
BLK <banigalapati@gmail.com>
2026-07-31 20:55:40 +0530

    Implement maxOperations to count K-sum pairs

---
+++ Max Number of K-Sum Pairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0, r = nums.length-1;
+        int cnt = 0;
+        while(l<r){
+            int sum = nums[l]+nums[r];
+            if(sum==k){
+                cnt++;
+                l++;
+                r--;
+            }
+            else if(sum<k)  l++;
+            else r--;
+        }
+        return cnt;
+    }
+}
