0bf9d41d3f4a54319dad5dde26d82484e8f7f80b
BLK <banigalapati@gmail.com>
2026-09-26 17:42:50 +0530

    Implement successfulPairs method for spells and potions

---
+++ Successful Pairs of Spells and Potions.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int[] ans = new int[spells.length];
+        for(int i=0;i<spells.length;i++){
+            int l = 0;
+            int r = potions.length;
+            while(l<r){
+                int m = l+(r-l)/2;
+                long pd = (long) spells[i] * potions[m];
+                if(pd>=success){
+                    r = m;
+
+                }
+                else l = m+1;
+            }
+            ans[i] = potions.length-l;
+        }
+        return ans;
+    }
+}
